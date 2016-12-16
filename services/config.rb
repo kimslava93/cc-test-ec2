300ea29945c1262582ef6af9cd0959a6fa6ac2a8
Kim Viacheslav <kimslava93@gmail.com>
2016-12-16 17:43:46 +0600

    updated config file

--- services/config.rb
+++ services/config.rb
@@ -19,7 +19,7 @@ coreo_aws_advisor_alert "check-ec2-sizes" do
   level "medium"
   audit_objects ["snapshot_set.volume_size", "snapshot_set.encrypted"]
   operators [">", "=="]
-  alert_when [8, true]
+  alert_when [8, false]
 end
 coreo_aws_advisor_ec2 "advise-ec2" do
   action :advise
